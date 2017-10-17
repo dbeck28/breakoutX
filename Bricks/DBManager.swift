f1e2e33ef09b43e8a31f81a6a731b62fd1a1fadb
Dayne Beck <daynebeck28@gmail.com>
2017-10-17 11:09:07 -0500

    Add SQLite3 Database with FMDB

---
+++ Bricks/DBManager.swift
@@ -0,0 +1,65 @@
+//
+//  DBManager.swift
+//  Bricks
+//
+//  Created by Dayne Beck on 10/17/17.
+//  Copyright © 2017 Dayne Beck. All rights reserved.
+//
+
+import UIKit
+
+class DBManager: NSObject {
+    // class to make singleton
+    static let shared: DBManager = DBManager()
+    
+    let field_Users_UserID = "userID"
+    let field_Users_UserName = "username"
+    let field_Users_Email = "email"
+    let field_Users_Password = "password"
+    
+    let databaseFileName = "database.sqlite"
+    
+    var pathToDatabase: String!
+    
+    var database: FMDatabase!
+    
+   
+    override init() {
+        super.init()
+        
+        let documentsDirectory = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString) as String
+        pathToDatabase = documentsDirectory.appending("/\(databaseFileName)")
+    }
+    
+    func createDatabase() -> Bool {
+        var created = false
+        
+            if !FileManager.default.fileExists(atPath: pathToDatabase) {
+            database = FMDatabase(path: pathToDatabase!)
+            
+            if database != nil {
+                // Open the database.
+                if database.open() {
+                    let createUsersTable = "create table users (\(field_Users_UserID) integer primary key autoincrement not null, \(field_Users_UserName) text not null, \(field_Users_Email) text not null, \(field_Users_Password) text not null)"
+                    
+                    
+                    do {
+                        try database.executeUpdate(createUsersTable, values: nil)
+                        created = true
+                    }
+                    catch {
+                        print("Could not create table.")
+                        print(error.localizedDescription)
+                    }
+                    
+                    // At the end close the database.
+                    database.close()
+                }
+                else {
+                    print("Could not open the database.")
+                }
+            }
+        }
+        return created
+    }
+}
