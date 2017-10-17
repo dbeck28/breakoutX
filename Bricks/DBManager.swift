bd3b453d836adb4e4f7c1b4b2cd4e453adac8f30
Dayne Beck <daynebeck28@gmail.com>
2017-10-17 11:13:45 -0500

    Add tokens to User model

--- Bricks/DBManager.swift
+++ Bricks/DBManager.swift
@@ -16,6 +16,7 @@ class DBManager: NSObject {
     let field_Users_UserName = "username"
     let field_Users_Email = "email"
     let field_Users_Password = "password"
+    let field_Users_Tokens = "tokens"
     
     let databaseFileName = "database.sqlite"
     
@@ -40,7 +41,7 @@ class DBManager: NSObject {
             if database != nil {
                 // Open the database.
                 if database.open() {
-                    let createUsersTable = "create table users (\(field_Users_UserID) integer primary key autoincrement not null, \(field_Users_UserName) text not null, \(field_Users_Email) text not null, \(field_Users_Password) text not null)"
+                    let createUsersTable = "create table users (\(field_Users_UserID) integer primary key autoincrement not null, \(field_Users_UserName) text not null, \(field_Users_Email) text not null, \(field_Users_Password) text not null), \(field_Users_Tokens) integer not nulls"
                     
                     
                     do {
